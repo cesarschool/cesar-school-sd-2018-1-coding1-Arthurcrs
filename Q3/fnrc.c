1f2e5d3e2bc59442543c6ace257a24117696d290
Arthurcrs <37412309+Arthurcrs@users.noreply.github.com>
2018-06-18 17:23:44 -0300

    Update fnrc.c

--- Q3/fnrc.c
+++ Q3/fnrc.c
@@ -10,13 +10,36 @@
  */
 char firstNonRepeatingChar (char sentence[], int length)
 {
-    printf("\nfirstNonRepeatingChar::START: %s, %d\n", s, length);
+    printf("\nfirstNonRepeatingChar::START: %s, %d\n", sentence, length);
     
-    // TODO: YOUR CODE HERE!
+    int isRepeated; //variable to check if the letter is diferent to all other letters in the same sentence
+    
+    for(int i = 0; i < length ; i++){ //looping for each letter in sentence
+        
+        isRepeated = 0; //initializing as 0 (false)
+        
+        //comparing current letter in position i with the letters in position j except for the one in the position i = j
+        for(int j = 0; j < length ; j++){
+            if (i != j){ 
+                if(sentence[i] == sentence[j]){ 
+                    isRepeated = 1; // if found another letter in the sentence that is equal to it
+                } //end if
+            } //end if
+        }// end for
+       
+        if (isRepeated==0) { //if no other letter is equal to it, isRepeated = 0
+            printf("%c", sentence[i]); //printing letter
+            printf("\nfirstNonRepeatingChar::END\n");
+            return sentence[i]; //returning the letter
+        }// end if
+        
+    }// end for
     
     printf("\nfirstNonRepeatingChar::END\n");
-    return -1;
-}
+    printf("-1");
+    return -1; 
+    
+}// end firstNonRepeatingChar
 
 int main()
 {
@@ -25,6 +48,5 @@ int main()
     firstNonRepeatingChar("ovo", 3);
     firstNonRepeatingChar("cesar school", 12);
     firstNonRepeatingChar("sistemas digitais", 17);
-    
     return 0;
 }
